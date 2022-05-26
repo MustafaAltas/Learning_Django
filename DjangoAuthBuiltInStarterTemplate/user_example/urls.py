2c5144412ee97038d60234fe623d76d05c0696dd
D1183-Mustafa <93746275+D1183-Mustafa@users.noreply.github.com>
2022-05-27 00:44:20 +0300

    auth işlemleri gerçekleştirildi.

---
+++ DjangoAuthBuiltInStarterTemplate/user_example/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import home_view, special,register
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('', home_view, name="home"),
+    path('special', special, name="special"),
+    path('register', register, name='register'),
+    path('change-password',auth_views.PasswordChangeView.as_view(template_name="registration/change-password.html") , name='change-password'),
+    path('reset-password/', auth_views.PasswordResetView.as_view(template_name='registration/reset-password.html'), name='reset-password'),
+]
---
+++ DjangoAuthBuiltInStarterTemplate/user_example/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+def home_view(request):
+    return render(request, "user_example/home.html")
+
+@login_required(login_url='/accounts/login/')
+def special(request):
+    return render(request, "user_example/special.html")
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+    form = UserCreationForm()
+    context = {
+        'form':form
+    }
+    return render(request, "registration/register.html", context)
