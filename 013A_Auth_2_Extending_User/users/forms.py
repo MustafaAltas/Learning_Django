1eb8e586098921b305e809be969df9711f5a7df9
D1183-Mustafa <93746275+D1183-Mustafa@users.noreply.github.com>
2022-05-30 00:26:15 +0300

    28.5.2022 dersi

---
+++ 013A_Auth_2_Extending_User/users/forms.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import User
+from .models import UserProfile
+
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+class UserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username','email')
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        # fields = '__all__'
+        exclude = ('user',)
+
---
+++ 013A_Auth_2_Extending_User/users/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render, redirect, HttpResponse
+
+from django.contrib import messages
+
+from django.contrib.auth import logout, login, authenticate
+from .forms import UserForm, UserProfileForm
+
+from django.contrib.auth.forms import AuthenticationForm
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'users/home.html')
+
+def user_logout(request):
+    messages.success(request, 'You logged out!')
+    logout(request)
+    return redirect('home')
+
+def register(request):
+    form_user = UserForm()
+    form_profile = UserProfileForm()
+
+    if request.method == 'POST':
+        form_user = UserForm(request.POST)
+        form_profile = UserProfileForm(request.POST, request.FILES)
+
+        if form_user.is_valid() and form_profile.is_valid():
+            user = form_user.save()
+            profile = form_profile.save(commit=False)
+            profile.user = user
+            profile.save()
+
+            login(request, user)
+            messages.success(request, 'Register Successfull!')
+
+            return redirect('home')
+
+    context = {
+        "form_user": form_user,
+        "form_profile": form_profile
+    }
+
+    return render(request, 'users/register.html',context)
+
+def user_login(request):
+    form = AuthenticationForm(request, data=request.POST)
+
+    if form.is_valid():
+        # username = form.cleaned_data.get('username')
+        # password = form.cleaned_data.get('password')
+        # user = authenticate(username=username, password = password)
+
+        user = form.get_user()
+
+        if user:
+            messages.success(request,'login successfull')
+            login(request, user)
+            return redirect('home')
+
+    return render(request, 'users/user_login.html', {"form":form})
